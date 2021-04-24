234ff40240aa2d64552d84014caf63154e179b8c
KenMwaura1 <kemwaura@gmail.com>
2021-04-24 23:19:07 +0300

    Updated sms notification file

--- stock_notification.py
+++ stock_notification.py
@@ -1,8 +1,16 @@
 from nse_scraper.models import StockData, db_connect
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import desc
-
 import africastalking as at
+import os
+from dotenv import load_dotenv
+
+load_dotenv('/nse_scraper/')
+at_username = os.getenv("at_username")
+at_api_key = os.getenv("at_api_key")
+mobile_number = os.getenv("mobile_number")
+at.initialize(at_username, at_api_key)
+sms = at.SMS
 
 engine = db_connect()
 Session = sessionmaker(bind=engine)
@@ -22,9 +30,19 @@ def stock_query():
         return new_data
 
 
-t = stock_query()
-print(t['stock_price'])
+# Create a function to send a message containing the stock ticker and price
 
 
-def stock_notification():
-    pass
+def stock_notification(stock_data: list, number: int):
+    try:
+        response = sms.send(stock_data, [number])
+        print(response)
+    except Exception as e:
+        print(f" Houston we have a problem: {e}")
+
+
+message = [stock_query()["stock_ticker"], stock_query()["stock_price"]]
+if stock_query().get("stock_price") >= 38.:
+    # Call the function passing the message  and mobile_number as a arguments
+    print(message)
+    stock_notification(str(message), mobile_number)
