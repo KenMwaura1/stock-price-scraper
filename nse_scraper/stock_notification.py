ad4b0068d8cb2d71c63a0674b0786209a6726cd4
KenMwaura1 <kemwaura@gmail.com>
2021-05-28 18:23:22 +0300

    refactored the code

--- nse_scraper/stock_notification.py
+++ nse_scraper/stock_notification.py
@@ -35,18 +35,18 @@ def stock_query():
 
 
 # Create a function to send a message containing the stock ticker and price
-def stock_notification(stock_data: list, number: int):
+def stock_notification(message: str, number: int):
     try:
-        response = sms.send(stock_data, [number])
+        response = sms.send(message, [number])
         print(account.fetch_application_data())
         print(response)
     except Exception as e:
         print(f" Houston we have a problem: {e}")
 
 
-message: list[Any] = [f'{stock_query()["stock_name"]}, is now Kes {stock_query()["stock_price"]} per share']
+message = f'{stock_query()["stock_name"]}, is now Kes {stock_query()["stock_price"]} per share'
 # check if Safaricom share price is more than Kes 39 and send a notification.
 if stock_query().get("stock_price") >= 38:
     # Call the function passing the message  and mobile_number as a arguments
     print(message)
-    stock_notification(str(message), mobile_number)
+    stock_notification(message, mobile_number)
